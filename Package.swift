ce95a9863821af15d0fb5332197408457f67228d
Arkadiusz Holko <fastred@fastred.org>
2019-08-31 17:05:28 +0200

    Fix Package.swift

--- Package.swift
+++ Package.swift
@@ -1,5 +1,12 @@
+// swift-tools-version:4.2
 import PackageDescription
 
 let package = Package(
-    name: "DeallocationChecker"
+    name: "DeallocationChecker",
+    products: [
+        .library(name: "DeallocationChecker", targets: ["DeallocationChecker-iOS"])
+    ],
+    targets: [
+        .target(name: "DeallocationChecker-iOS", dependencies: [], path: "Sources")
+    ]
 )
